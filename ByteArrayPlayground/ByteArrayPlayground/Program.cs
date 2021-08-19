3382e7c280bf6ed3acb712210941f84f1c54b6f8
gshukov98 <56069656+gshukov98@users.noreply.github.com>
2021-08-19 15:28:35 +0300

    Byte Converter

---
+++ ByteArrayPlayground/ByteArrayPlayground/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ByteArrayPlayground
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var i = 2147483647;
+            var resultBytes = ConvertIntToByteArray(i);
+            Console.WriteLine($"input: {i} bytes: {BitConverter.ToString(resultBytes)}");
+
+            var byteToInt = ConvertByteArrayToInt(resultBytes);
+            Console.WriteLine($"input: {BitConverter.ToString(resultBytes)} int: {byteToInt}");
+
+            Console.ReadLine();
+        }
+
+        public static byte[] ConvertIntToByteArray(int param)
+        {
+            return BitConverter.GetBytes(param);
+        }
+        public static int ConvertByteArrayToInt(byte[] param)
+        {
+            return BitConverter.ToInt32(param, 0);
+        }
+    }
+}
